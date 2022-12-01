82185c5d2b30dd2d3602bcd71286a15e6e6580af
Noah van Ekdom <noahvanekdom@gmail.com>
2022-12-01 15:00:49 -0700

    Set up fly.io deployment

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -9,6 +9,8 @@ Rails.application.configure do
   # Do not eager load code on boot.
   config.eager_load = false
 
+  config.assets.precompile += ['graphiql/rails/application.js', 'graphiql/rails/application.css']
+
   # Show full error reports.
   config.consider_all_requests_local = true
 
