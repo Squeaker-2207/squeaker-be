3f8a4962e8761c167e222677a43bb473d47c8753
Gavin Carew <gjcarew@gmail.com>
2022-12-01 13:38:40 -0800

    Add is_admin column to users, default value is false

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_30_225545) do
+ActiveRecord::Schema.define(version: 2022_12_01_210449) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2022_11_30_225545) do
     t.string "username"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "is_admin", default: false
   end
 
 end
