bb0dab059e3303cddca7d17d225e51f524b8cb2a
Gavin Carew <gjcarew@gmail.com>
2022-12-01 19:55:00 -0800

    Tests for follow and unfollow methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,12 @@
 class User < ApplicationRecord
   validates :username, presence: true, uniqueness: true
   has_many :squeaks
-  has_many :active_relationships, class_name:  'Relationship',
+  has_many :active_relationships, class_name: 'Relationship',
                                   foreign_key: 'follower_id',
                                   dependent: :destroy
+  has_many :passive_relationships, class_name: 'Relationship',
+                                   foreign_key: 'followed_id',
+                                   dependent: :destroy
   has_many :following, through: :active_relationships, source: :followed
   has_many :followers, through: :passive_relationships, source: :follower
 
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,11 +9,24 @@ RSpec.describe User, type: :model do
 
   describe 'instance methods' do
     describe '#follow' do
-
+      it 'One user should be able to follow another user' do 
+        users = create_list(:user, 2)
+        expect(users[0].followers).to be_empty
+        users[1].follow(users[0])
+        expect(users[0].followers.include?(users[1])).to be(true)
+      end
     end
 
     describe '#unfollow' do
-
+      it 'A user should be able to unfollow another user' do 
+        users = create_list(:user, 2)
+        expect(users[0].followers).to be_empty
+        users[1].follow(users[0])
+        expect(users[0].followers.include?(users[1])).to be(true)
+        
+        users[1].unfollow(users[0])
+        expect(users[0].followers).to be_empty
+      end
     end
   end
 end
