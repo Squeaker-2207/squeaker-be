7a5939df2e2c8567a30e3a7dab456b101821deab
Ken Lenhart <omnipaid@gmail.com>
2022-12-01 21:25:54 -0800

    Add user factory, add test for fetch_users

--- spec/graphql/users/users_spec.rb
+++ spec/graphql/users/users_spec.rb
@@ -1,7 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe 'Users Query' do
+  let(:users) { create_list(:user, 5) }
+
   it 'returns a list of users' do
-    
+    query = <<~GQL
+      query {
+        fetchUsers {
+          id
+          username
+          isAdmin
+        }
+      }
+    GQL
+
+    result = SqueakrBeSchema.execute(query, context: { fetch_users: users })
+
+    expect(result.dig("data")).to be_a(Hash)
+    expect(result.dig("data", "fetchUsers")).to be_a(Array)
+
+    result.dig("data", "fetchUsers").each do |user|
+      expect(user["id"]).to be_a(String)
+      expect(user["username"]).to be_a(String)
+      expect(user["isAdmin"]).to be_in([true, false])
+    end
   end
 end
