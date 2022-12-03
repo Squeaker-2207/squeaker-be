9b5a97862f6c77d3d5f3b312e5b4fcabc81615b8
Ken Lenhart <omnipaid@gmail.com>
2022-12-03 15:55:02 -0800

    Add add_user mutation method, add add_user field to mutation_type.rb

--- app/graphql/queries/fetch_user.rb
+++ app/graphql/queries/fetch_user.rb
@@ -3,10 +3,10 @@ module Queries
     type Types::UserType, null: false
     argument :id, ID, required: true
 
-    def resolve(id)
+    def resolve(id:)
       User.find(id)
     rescue ActiveRecord::RecordNotFound => e 
-      GraphQL::ExecutionError.new('User with that id not found.')
+      GraphQL::ExecutionError.new("User with id #{id} not found.")
     rescue ActiveRecord::RecordInvalid => e 
       GraphQL::ExecutionError.new("Invalid Attributes for #{e.record.class}:"\
       " #{e.record.class.errors.full_messages.join(', ')}")
