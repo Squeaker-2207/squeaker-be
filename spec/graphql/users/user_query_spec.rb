32a360f5b964b735cc3d67f97f6ab16b42eaac6a
Ken Lenhart <omnipaid@gmail.com>
2022-12-03 16:50:53 -0800

    Add error test for user id not found

--- spec/graphql/users/user_query_spec.rb
+++ spec/graphql/users/user_query_spec.rb
@@ -2,7 +2,6 @@ require 'rails_helper'
 
 RSpec.describe 'Get One User by ID Query' do
   let(:user) { create(:user)}
-  let(:bad_user) { create(:user, username: nil )}
 
   it 'returns user by id' do
     query = <<~GQL
@@ -17,9 +16,29 @@ RSpec.describe 'Get One User by ID Query' do
 
     result = SqueakrBeSchema.execute(query, context: { fetch_user: user })
 
-    expect(result.dig("data").to be_a(Hash))
+    expect(result.dig("data")).to be_a(Hash)
     expect(result.dig("data", "fetchUser")).to be_a(Hash)
     expect(result.dig("data", "fetchUser", "id")).to be_a(String)
-    expect(result.dig("data, fetchUser", "id")).to eq("#{user.id}")
+    expect(result.dig("data", "fetchUser", "id")).to eq("#{user.id}")
+    expect(result.dig("data", "fetchUser", "username")).to eq("#{user.username}")
+    expect(result.dig("data", "fetchUser", "isAdmin")).to eq(user.is_admin)
+  end
+
+  it 'returns error if user id is not found' do
+    query = <<~GQL
+      query {
+        fetchUser(id: "999") {
+          id
+          username
+          isAdmin
+        }
+      }
+    GQL
+
+    result = SqueakrBeSchema.execute(query)
+
+    expect(result.dig("data")).to eq(nil)
+    expect(result.dig("errors")).to be_a(Array)
+    expect(result.dig("errors", 0, "message")).to eq("User with id 999 not found.")
   end
 end
