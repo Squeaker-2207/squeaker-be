9ad676bd71df10e1edd7accb029565d114137639
Gavin Carew <gjcarew@gmail.com>
2022-12-07 17:19:49 -0800

    Add validation for hate speech to Squeak class

--- app/models/squeak.rb
+++ app/models/squeak.rb
@@ -1,4 +1,14 @@
 class Squeak < ApplicationRecord
   belongs_to :user
+  validates :content, presence: true
+  validate :filter
 
+  private
+
+  def filter
+    label = NyckelService.get_label(self.content)
+    if label[:labelName] == 'Hate Speech' && label[:confidence] > 0.8
+      self.errors.add(:content, label[:labelName])
+    end
+  end
 end
