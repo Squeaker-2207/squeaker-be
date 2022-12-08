136cabc6d9e545d23174f5431ad6c2caa5d8b436
Gavin Carew <gjcarew@gmail.com>
2022-12-07 17:22:14 -0800

    Validate presence of content

--- spec/models/squeak_spec.rb
+++ spec/models/squeak_spec.rb
@@ -3,5 +3,8 @@ require 'rails_helper'
 RSpec.describe Squeak, type: :model do
   describe 'validations' do
     it { should belong_to :user }
+    it { should validate_presence_of :content }
+
+
   end
 end
