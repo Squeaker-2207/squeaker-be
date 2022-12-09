fa1dab495f634b0dc3bdc74bea7841af765539e4
Noah van Ekdom <noahvanekdom@gmail.com>
2022-12-09 10:23:29 -0700

    Add error testing

--- spec/graphql/squeaks/update_squeak_spec.rb
+++ spec/graphql/squeaks/update_squeak_spec.rb
@@ -14,6 +14,7 @@ RSpec.describe 'Update Squeak Mutation' do
             id
             content
             reports
+            nuts
             score{
               metric
               probability
@@ -46,15 +47,39 @@ RSpec.describe 'Update Squeak Mutation' do
 
     it "reporting a squeak does not change any other squeak values" do
       result = SqueakrBeSchema.execute(@query)
-      expect(result.dig("data")).to be_a(Hash)
       squeak_return = result.dig("data", "updateSqueak", "squeak")
       expect(squeak_return.dig("content")).to eq(@squeak.content)
+      expect(squeak_return.dig("nuts")).to eq(@squeak.nuts)
     end
 
     xit "a user cannot report a squeak more than once " do
       ##requires refactor before possible
       ##repeat previous steps twice to test, requires schema changes to succeed
 
+    end
+
+    it "raises an error if the squeak cannot be found" do
+      query_v2 = <<~GQL
+      mutation {
+        updateSqueak(input: { id: 1212312312421124, report: true }) {
+          squeak {
+            id
+            content
+            reports
+            nuts
+            score{
+              metric
+              probability
+            }
+          }
+        }
+      }
+      GQL
+      result = SqueakrBeSchema.execute(query_v2)
+      errors = result.dig("errors")
+      expect(errors).to be_an Array
+      expect(errors.first.dig("message")).to be_a String
+      expect(errors.first.dig("path")).to eq(["updateSqueak"])
     end
   end
 
@@ -67,6 +92,7 @@ RSpec.describe 'Update Squeak Mutation' do
             id
             content
             nuts
+            reports
             }
           }
         }
@@ -74,18 +100,47 @@ RSpec.describe 'Update Squeak Mutation' do
     end
 
     it 'can add a nut to a squeak' do
+      expect(@squeak.nuts).to eq 1
       result = SqueakrBeSchema.execute(@query)
       expect(result.dig("data")).to be_a(Hash)
       expect(result.dig("data", "updateSqueak", "squeak")).to be_a(Hash)
       squeak_return = result.dig("data", "updateSqueak", "squeak")
+      expect(squeak_return.dig("nuts")).to eq 2
     end
 
     it 'does not modify the other values of a squeak when nutting' do
-
+      result = SqueakrBeSchema.execute(@query)
+      squeak_return = result.dig("data", "updateSqueak", "squeak")
+      expect(squeak_return.dig("content")).to eq(@squeak.content)
+      expect(squeak_return.dig("reports")).to eq(@squeak.reports)
     end
 
     xit 'cannot nut a squeak more than once' do
       ##currently planning to be handled by front end, best way to handle is described above
+    end
+
+    it 'returns an error when the squeak cannot be found' do
+      bad_query = <<~GQL
+      mutation {
+        updateSqueak(input: { id: 1212312312421124, nut: true }) {
+          squeak {
+            id
+            content
+            reports
+            nuts
+            score{
+              metric
+              probability
+            }
+          }
+        }
+      }
+      GQL
+      result = SqueakrBeSchema.execute(bad_query)
+      errors = result.dig("errors")
+      expect(errors).to be_an Array
+      expect(errors.first.dig("message")).to be_a String
+      expect(errors.first.dig("path")).to eq(["updateSqueak"])
     end
   end
 end
