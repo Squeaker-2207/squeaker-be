2ad9bd5975baad0643dfdaa97a20cfc4c6e30d8f
Anna Marie Sterling <sterling.am@gmail.com>
2022-12-08 17:05:22 -0700

    Add vcr to API dependent tests

--- app/models/squeak.rb
+++ app/models/squeak.rb
@@ -3,15 +3,16 @@ class Squeak < ApplicationRecord
   validates :content, presence: true
   validate :filter
 
+  scope :reported, -> { where('reports > 0') }
+
+  def score
+    ScoreFetcher.fetch_score(self)
+  end
+
   def filter
     label = NyckelService.get_label(self.content)
     if label[:labelName] == 'Hate Speech' && label[:confidence] > 0.8
       self.errors.add(:content, label[:labelName])
     end
-  end
-  scope :reported, -> { where('reports > 0') }
-
-  def score
-    ScoreFetcher.fetch_score(self)
   end
 end
