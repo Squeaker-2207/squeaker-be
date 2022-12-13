29060e77c415a2d7456961f8e122445b71609f17
Ken Lenhart <omnipaid@gmail.com>
2022-12-13 13:05:21 -0800

    Add post request testing for graphql fetch_users method

--- spec/graphql/queries/users/post_all_users_query_spec.rb
+++ spec/graphql/queries/users/post_all_users_query_spec.rb
@@ -5,7 +5,47 @@ module Queries
     RSpec.describe 'Post Request for All Users', type: :request do
       let(:users) { create_list(:user, 5) }
 
-      it ''
+      it 'makes successful post request when fetching all users' do
+        post graphql_path, params: { query: query }
+
+        expect(response.status).to eq(200)
+      end
+
+      it 'has expected data for all users' do
+        post graphql_path, params: { query: query, context: { fetch_users: users } }
+
+        json = JSON.parse(response.body, symbolize_names: true)
+
+        users_data = json[:data][:fetchUsers]
+
+        users_data.each do |user|
+          expect(user[:id]).to be_a(String)
+          expect(user[:username]).to be_a(String)
+          expect(user[:isAdmin]).to eq(false)
+        end
+      end
+
+      it 'returns empty array if no users are found' do
+        post graphql_path, params: { query: query }
+
+        json = JSON.parse(response.body, symbolize_names: true)
+
+        users_data = json[:data][:fetchUsers]
+
+        expect(users_data).to eq([])
+      end
+
+      def query
+        <<~GQL
+          query {
+            fetchUsers {
+              id
+              username
+              isAdmin
+            }
+          }
+        GQL
+      end
     end
   end
 end
