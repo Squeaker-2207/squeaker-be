2f1a0bbd42688415fe88a2a053d2223da66ca23d
Ken Lenhart <omnipaid@gmail.com>
2022-12-13 12:45:31 -0800

    Add post_all_users_query_spec.rb

---
+++ spec/graphql/queries/users/post_all_users_query_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+module Queries
+  module Users
+    RSpec.describe 'Post Request for All Users', type: :request do
+      let(:users) { create_list(:user, 5) }
+
+      it ''
+    end
+  end
+end
