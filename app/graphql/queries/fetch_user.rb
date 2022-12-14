189c6df19553c67a103a0fca84c311f763d4217d
Ken Lenhart <omnipaid@gmail.com>
2022-12-14 09:11:37 -0800

    Add all_squeaks field to graphql user type, add associated tests

--- app/graphql/queries/fetch_user.rb
+++ app/graphql/queries/fetch_user.rb
@@ -1,10 +1,12 @@
 module Queries
   class FetchUser < Queries::BaseQuery
     type Types::UserType, null: false
+
     argument :id, String, required: true
 
     def resolve(id:)
       User.find(id)
+
     rescue ActiveRecord::RecordNotFound => e 
       GraphQL::ExecutionError.new("User with id #{id} not found.")
     end
--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -8,9 +8,14 @@ module Types
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
     field :user_squeaks, [Types::SqueakType], null: false
+    field :all_squeaks, [Types::SqueakType], null: false
 
     def user_squeaks
       object.squeaks
+    end
+
+    def all_squeaks
+      Squeak.permitted.order(created_at: :desc)
     end
   end
 end
